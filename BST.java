2b64fded2f4aae37feee2e16e61ebe68b7118e66
john-fike <johnfike223@gmail.com>
2023-02-19 14:37:25 -0700

    first

---
+++ BST.java
@@ -0,0 +1,101 @@
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ *
+ * @author yaw
+ */
+public class BST {
+
+    Node root;
+    ArrayList<Node> inorderNodes = new ArrayList<>();
+    public BST(){
+        root = null;
+    }
+    public void insert(int newValue) {
+        if (root == null) {
+            root = new Node(newValue);
+        } else {
+            Node currentNode = root;
+            boolean placed = false;
+            while (!placed) {
+                if (newValue == currentNode.getValue()) {
+                    placed = true;
+                    // Don't insert repeated value.
+                } else if (newValue < currentNode.getValue()) {
+                    if (currentNode.getLeft() == null) {
+                        currentNode.setLeft(new Node(newValue));
+                        currentNode.getLeft().setParent(currentNode);
+                        placed = true;
+                    } else {
+                        currentNode = currentNode.getLeft();
+                    }
+                } else {
+                    if (currentNode.getRight() == null) {
+                        currentNode.setRight(new Node(newValue));
+                        currentNode.getRight().setParent(currentNode);
+                        placed = true;
+                    } else {
+                        currentNode = currentNode.getRight();
+                    }
+                }
+            }
+        }
+    }
+    public void writeToFile() {
+        Queue<Node> queue = new LinkedList<>();
+        try {
+            PrintWriter outFile = new PrintWriter("tree.txt");
+            if (root != null) {
+                queue.add(root);
+                while (!queue.isEmpty()) {
+                    Node node = queue.remove();
+                    System.out.println("Writing " + node.getValue() + " to file...");
+                    outFile.print(node.getValue() + ",");
+                    System.out.println("Successfully wrote to the file.");
+                    if(node.getLeft()!=null){queue.add(node.getLeft());}
+                    if(node.getRight()!=null){queue.add(node.getRight());}
+                }
+            }
+            outFile.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    }
+    public void loadFromFile() {
+        try {
+            FileReader file = new FileReader("tree.txt");
+            Scanner inFile = new Scanner(file);
+            String tree = inFile.nextLine();
+            System.out.println("Loaded: " + tree);
+            String[] tokens = tree.split(",");
+            for (String s : tokens){
+                insert(Integer.parseInt(s));
+            }
+            inFile.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    }
+    public void reNumber(Node node){
+        if(node!=null){
+            reNumber(node.getLeft());
+            inorderNodes.add(node);
+            System.out.println("Adding " + node.getValue());
+            reNumber(node.getRight());
+        }else{
+            for(int i=0; i<inorderNodes.size()-1; i++){
+                inorderNodes.get(i).setValue(i);
+                System.out.print(inorderNodes.get(i).getValue() + " ");
+            }
+        }
+
+    }
+    public void reNumber() {
+        reNumber(root);
+    }
+}
---
+++ Test.java
@@ -0,0 +1,37 @@
+
+
+/**
+ *
+ * @author yaw
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        BST bst = new BST();
+        bst.insert(44);
+        bst.insert(17);
+        bst.insert(88);
+        bst.insert(8);
+        bst.insert(32);
+        bst.insert(65);
+        bst.insert(97);
+        bst.insert(27);
+        bst.insert(54);
+        bst.insert(82);
+        bst.insert(93);
+        bst.insert(21);
+        bst.insert(29);
+        bst.insert(76);
+        bst.insert(68);
+        bst.insert(80);
+
+        bst.writeToFile();
+        bst.loadFromFile();
+        bst.writeToFile();
+        bst.reNumber();
+        bst.writeToFile();
+    }
+}
